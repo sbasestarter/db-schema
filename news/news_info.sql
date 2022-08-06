3d39cd7aad07cca43c7a6abfc3d34b74d6b095d3
patdz <patdz@ymipro.com>
2022-08-06 16:18:33 +0800

    update news_info

--- news/news_info.sql
+++ news/news_info.sql
@@ -5,6 +5,6 @@ CREATE TABLE `news_info` (
   `images_json` text NOT NULL,
   `created_at` datetime NOT NULL,
   `published_at` datetime NOT NULL,
-  `category_id` bigint NOT NULL,
+  `category_id` int NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
