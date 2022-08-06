0a79907edaa2c9ca36977a7e8ffdbfc23bbfd9e1
patdz <patdz@ymipro.com>
2022-08-06 16:13:23 +0800

    update news_info

--- news/news_content.sql
+++ news/news_content.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `news_content` (
-  `id` int NOT NULL,
+  `id` bigint NOT NULL,
   `content` mediumtext,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
--- news/news_info.sql
+++ news/news_info.sql
@@ -1,10 +1,10 @@
 CREATE TABLE `news_info` (
-  `id` int NOT NULL AUTO_INCREMENT,
+  `id` bigint NOT NULL AUTO_INCREMENT,
   `title` varchar(512) NOT NULL,
   `summary` text NOT NULL,
   `images_json` text NOT NULL,
   `created_at` datetime NOT NULL,
   `published_at` datetime NOT NULL,
-  `category_id` int NOT NULL,
+  `category_id` bigint NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
